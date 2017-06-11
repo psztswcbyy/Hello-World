d168b1278d4f23a5a9d5216a8f8545043dfa6439
Ageu Matheus <ageu_mateus@outlook.com>
2017-06-11 13:59:16 +0100

    Let's make tic tac toe game -  first commit

---
+++ simple_tic_tac_toe_game.py
@@ -0,0 +1,19 @@
+# Let's make a simple tic tac toe game with python
+
+# scoreboard
+
+def scoreboard():
+	print(" ============================ ")
+	print("|         TIC TAC TOE        |")
+	print("|============================|")
+	print("|  X = 43      VS    O = 12  |")
+
+# verify if game is finish
+
+# game on
+
+while True:
+
+	scoreboard()
+
+	break
